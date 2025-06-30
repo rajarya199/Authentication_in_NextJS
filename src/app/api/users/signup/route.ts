995e6fe01f2d6d45149fbc0cb0f9bc8dcaf307b9
rajarya199 <aryalrajan078@gmail.com>
2025-06-30 22:56:34 +0545

    reg route

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,42 @@
+import User from'@/models/userModel'
+import { connect } from '@/db/dbConfig'
+import{NextRequest,NextResponse } from 'next/server'
+import bcrypt from 'bcryptjs'
+import { sendEmail } from '@/utils/mailer'
+connect()
+export async function POST(request:NextRequest){
+    try{
+  const reqBody= await request.json()
+  const{username,email,password}=reqBody
+  //validate
+  console.log(reqBody);
+  const user=await User.findOne({email})
+  if(user){
+    return NextResponse.json({error:"User already exists"},{status:400})
+  }
+const salt= await bcrypt.genSaltSync(10);
+const hashedPassword=await bcrypt.hash(password,salt)
+ const newUser=new User({
+    username,
+    email,
+    password:hashedPassword
+})
+const savedUser=await newUser.save()
+console.log(savedUser)
+
+//send verification email 
+await sendEmail({email,emailType:'VERIFY',userID:savedUser._id})
+return NextResponse.json({
+  message:"User created successfully",
+success:true,
+savedUser},
+  {status:201})
+    }
+    catch(error:any){
+              console.error(error)
+
+        return NextResponse.json({error:error.message},{status:500})
+    }
+
+}
+
